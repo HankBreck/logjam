35c21d28117bc8df86cf26712ea4b3ce83a3dc3d
HankBreck <breckenridgeh2@gmail.com>
2023-06-06 15:41:33 -0600

    WIP: Adding first DAG to airflow

--- data-service/dags/snow.py
+++ data-service/dags/snow.py
@@ -1,2 +1,11 @@
+import os
+from datetime import timedelta
+
+import airflow
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+dag_path = os.getcwd()
+
 # TODO: Setup first DAG to pull snow data
     # https://www.youtube.com/watch?v=2nhdhIYueIE
